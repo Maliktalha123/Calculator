a6a275daf1dc61cf974bb251b656eec5e2a730af
Maliktalha123 <talhanoormalik0321@gmail.com>
2024-04-17 17:58:22 +0500

    This is a Calculator App

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let calculatorInput = document.getElementsByClassName("input");
+function one(){
+    calculatorInput.innerHTML = '1';
+    console.log("clicked")
+}
+let heading = document.getElementsByClassName("heading");
+heading.innerHTML += "Im amnfajzn";
