eb1cf09a637758b8c896078b2eba5e0000e9a1c0
Maliktalha123 <talhanoormalik0321@gmail.com>
2024-05-08 22:12:38 +0500

    There is my project Completed.

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-let string = "";
-let buttons = document.querySelectorAll(".button");
-Array.from(buttons).forEach((button) => {
-  button.addEventListener("click", (e) => {
-    if (e.target.innerHTML == "=") {
-      string = eval(string)
-      document.querySelector("input").value = string;
-    } else if (e.target.innerHTML == "C") {
-      string = ""
-      document.querySelector("input").value = string;
-    } else {
-      string = string + e.target.innerHTML;
-      document.querySelector("input").value = string;
+// let string = "";
+// let buttons = document.querySelectorAll(".button");
+// Array.from(buttons).forEach((button) => {
+//   button.addEventListener("click", (e) => {
+//     if (e.target.innerHTML == "=") {
+//       string = eval(string);
+//       document.querySelector("input").value = string;
+//     } else if (e.target.innerHTML == "C") {
+//       string = "";
+//       document.querySelector("input").value = string;
+//     } else {
+//       string = string + e.target.innerHTML;
+//       document.querySelector("input").value = string;
+//     }
+//   });
+// });
+
+let outputScreen = document.getElementById("input");
+
+function operation(num){
+    outputScreen.value += num;
+
+}
+function calculate(){
+    try{
+        outputScreen.value = eval(outputScreen.value);
     }
-  });
-});
+    catch(err)
+    {
+        alert("Invalid Input")
+    }
+}
+function Clear(){
+    outputScreen.value = ""
+}
+function del(){
+    outputScreen.value = outputScreen.value.slice(0,-1)
+}
